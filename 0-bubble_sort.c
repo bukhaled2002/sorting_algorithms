7a62beb6cc44253c324e1a1e508beddf3d8dcbdd
bukhaled2002 <bakhaled.310@gmail.com>
2023-09-24 03:15:52 -0700

    f

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,33 +8,34 @@
  */
 void swap(int *n1, int *n2)
 {
-    int temp = *n1;
-    *n1 = *n2;
-    *n2 = temp;
+	int temp = *n1;
+	*n1 = *n2;
+	*n2 = temp;
 }
 
 /**
  * bubble_sort - Sort an array of integers in ascending order.
- * @arr: An array of integers to sort.
- * @n: The size of the array.
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
  *
  * Description: Prints the array after each swap.
  */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-    if (array == NULL || size < 2)
-        return;
 
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < size - i - 1; j++)
-        {
-            if (array[j] >= array[j + 1])
-            {
-                swap(&array[j], &array[j + 1]);
-		print_array(array, size);
-            }
-        }
-    }
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] >= array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
+		}
+	}
 }
