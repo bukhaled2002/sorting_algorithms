958e3b0e80499bc8182337de075684c5670c1154
bukhaled2002 <bakhaled.310@gmail.com>
2023-09-24 02:48:14 -0700

    gg

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -19,13 +19,18 @@ void swap(int *n1, int *n2){
  *
  * Description: Prints the array after each swap.
  */
-void bubble_sort(int arr[], int n){
-    int i, j;
-    for (i = 0; i < n; i++) {
-        for(j=0;j<n-i-1;j++){
-            if(arr[j]>=arr[j+1]){
-                swap(&arr[j], &arr[j+1]);
-            }
-        }
-    }
+void bubble_sort(int arr[], int n)
+{
+	if (arr == NULL || n < 2)
+		return;
+	int i, j;
+    	for (i = 0; i < n; i++) {
+        	for(j=0;j<n-i-1;j++)
+		{
+            		if(arr[j]>=arr[j+1])
+			{
+                		swap(&arr[j], &arr[j+1]);
+            		}
+        	}
+    	}
 }
